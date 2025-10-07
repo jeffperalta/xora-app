3bf181a3c2cd515b46d52fdbd45c77357cb9cb09
Jeff Peralta <jaydocperalta@gmail.com>
2025-10-07 11:39:08 +1100

    Price modification

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
               Video Editing
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly simple
+              Amazingly simple!
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
               We designed XORA AI Video Editor to be an easy to use, quick to
